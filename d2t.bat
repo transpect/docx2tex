91f2e08d3371951d18c4444365f0da05dd6b0edf
mkraetke <mkraetke@gmx.de>
2015-04-27 10:07:23 +0200

    add leightweight batch script

---
+++ d2t.bat
@@ -0,0 +1,55 @@
+@echo off 
+
+REM command line parameters
+@set FILE=%~dpnx1
+@set CONF=%~dpnx2
+
+IF [%FILE%] == [] GOTO usage
+
+REM get basename
+@set BASENAME=%~n1
+
+REM script directory
+@set sd=%~dp0
+@set DIR=%sd:\=/%
+
+IF [%CONF%] == [] @set CONF=%DIR%/conf/conf.xml
+
+REM output directory
+@set sd=%~dp1
+@set OUT_DIR=%sd:\=/%
+
+REM script parameters
+@set JAVA=java
+@set CALABASH=calabash/calabash.bat
+
+REM convert backward slash to slash
+@set FILE=%FILE:\=/%
+@set CONF=%CONF:\=/%
+@set OUT_DIR=%OUT_DIR:\=/%
+
+REM debugging
+@set DEBUGDIR_URI=file:/%OUT_DIR%/%BASENAME%.debug
+@set LOG=%OUT_DIR%%BASENAME%.log
+
+REM start 
+echo starting docx2tex
+call %CALABASH% -i conf=%CONF% -o result=%OUT_DIR%/%BASENAME%.tex -o hub=%OUT_DIR%/%BASENAME%.xml %DIR%/xpl/docx2tex.xpl docx=%FILE% debug=yes debug-dir-uri=%DEBUGDIR_URI% 2>&1 2>>%LOG% || GOTO exitonerror
+
+goto finish
+
+REM exit with errors
+:exitonerror
+echo Errors encountered while running docx2tex. Please see %LOG% for details.
+exit 1
+
+REM exit
+:finish
+echo docx2tex finished. Output written to %OUT_DIR%/%BASENAME%.tex.
+exit 0
+
+REM Sample invocation:
+:usage
+echo docx2tex
+echo Usage: d2t.bat <docx file> <config file>
+exit 1
